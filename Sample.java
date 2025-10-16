09349b328aa7142844890127dc9d2894b31c972d
singareyash <singareyash.11@gmail.com>
2025-10-16 14:50:00 +0530

    Fix typo and update output message in Sample.java

--- Sample.java
+++ Sample.java
@@ -3,6 +3,6 @@ class Sample
 public static void main(String []args)
 {
 System.out.println("Hello wrorld ");
-  System.out.println("hello devops Engineer");
+  System.out.println("hello devops Engineer ...................");
 }
 }
